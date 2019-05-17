d334031b86754d4a54d420a9c2dad99f43c62a84
SamanthaLossett <samanthalossett@gmail.com>
2019-05-17 08:58:05 -0700

    with the giphy ruery URL but local storage still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,18 @@
 var topics = ["Game of Thrones", "Handmaids Tale", "Big Little Lies"];
-var whatUserTypes = localStorage.getItem("#added");
+var whatUserTypes = localStorage.getItem("#user-input");
 var moviesAdded = 0;
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=ZzkFtzViT9yOr6IEBOCtJxV7nBoghEVi&limit=10";
+      
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
 
-var queryURL = 
 $(document).ready(function () {       //"now start loading javasctipt and JQ"//
     console.log("ready!");
 
     function appendButtons() {    
-        $("#buttons").empty();  // this makes everything in TOPICS array a button//
+        $("#buttons").empty();  // this makes everything in TOPICS array into a button//
         for (var i = 0; i < topics.length; i++) {
             $("#buttons").append("<button>" + topics[i] + "</button>");
             console.log(topics);
@@ -15,15 +20,17 @@ $(document).ready(function () {       //"now start loading javasctipt and JQ"//
     }
     appendButtons();
 
-    $("#add-user-input").on("click", function (event) {
+    $("#add-user-input").on("click", function (event) {    //this event makes users tet into a button in the existing array, then clears //
         event.preventDefault();
         console.log("the onclick is working");
         var whatUserTypes = $("#user-input").val().trim();
         console.log(whatUserTypes);
         topics.push(whatUserTypes);
         $("#user-input").val(" ");
-        localStorage.setItem("#added", topics);
+        localStorage.setItem("#user-input", whatUserTypes);
+
         moviesAdded++;
         appendButtons();
     })
+$("#user-input").text(whatUserTypes.getItem());
 });
